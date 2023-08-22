808402b89c22ac01f37e2d63dad704a92d5953f7
Eli <Elayjahh7@hotmail.com>
2023-08-22 20:12:22 +1200

    Renamed AGAIN

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Lana-Renee-Lashes")]
+[assembly: AssemblyTitle("LanaReneeLashes")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Lana-Renee-Lashes")]
+[assembly: AssemblyProduct("LanaReneeLashes")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
